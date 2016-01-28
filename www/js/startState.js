9525598183f94145c509f9cbdd1ae065944e23bf
Wojciech Langiewicz <wlangiewicz@gmail.com>
2016-01-28 14:42:48 +0100

    change desired fps to 30

--- www/js/startState.js
+++ www/js/startState.js
@@ -4,7 +4,7 @@ var startState = {
 
     preload: function () {
         game.renderer.renderSession.roundPixels = true;
-
+        game.time.desiredFps = 30;
 
         game.stage.backgroundColor = '#71c5cf';
         game.load.image('coin', 'assets/bitcoin.png');
